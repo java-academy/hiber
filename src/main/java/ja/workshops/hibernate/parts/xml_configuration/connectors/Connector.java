706a193fc4030862901712fad0d4c982912804dd
Ola <aleksandra.podorska@gmail.com>
2019-06-13 09:56:01 +0200

    Update xml configuration task: - add Connector and App class - add readme.adoc - change URL, USER, PASS properties in H2 connection

---
+++ src/main/java/ja/workshops/hibernate/parts/xml_configuration/connectors/Connector.java
@@ -0,0 +1,13 @@
+package ja.workshops.hibernate.parts.xml_configuration.connectors;
+
+import org.hibernate.Session;
+
+/**
+ * @author Ola Podorska
+ */
+public class Connector {
+
+    public static Session getSession(){
+        return null;
+    }
+}
