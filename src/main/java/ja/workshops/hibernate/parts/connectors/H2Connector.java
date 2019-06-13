30776761e18d01024763e8b1c206c5e11df8fbc1
Krzysiek <krzysztof.niedzielski96@gmail.com>
2019-06-13 09:51:30 +0200

    add queries task: - add query package with 4 different query types - add simple tests to query types classes - add app - add readme.adoc - add string field to h2connector class to modify of ddl-auto behaviour

--- src/main/java/ja/workshops/hibernate/parts/connectors/H2Connector.java
+++ src/main/java/ja/workshops/hibernate/parts/connectors/H2Connector.java
@@ -11,6 +11,17 @@ import java.util.Properties;
  * @author krzysztof.niedzielski
  */
 public class H2Connector extends SessionConnector {
+
+    private String ddlAuto;
+
+    public H2Connector(String ddlAuto) {
+        this.ddlAuto = ddlAuto;
+    }
+
+    public H2Connector() {
+        ddlAuto="create-drop";
+    }
+
     @Override
     Properties loadConnectorSettings() {
         Properties settings = new Properties();
@@ -20,7 +31,7 @@ public class H2Connector extends SessionConnector {
         settings.put(Environment.PASS, "{password}");
         settings.put(Environment.DIALECT, "org.hibernate.dialect.H2Dialect");
         settings.put(Environment.SHOW_SQL, "true");
-        settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+        settings.put(Environment.HBM2DDL_AUTO, ddlAuto);
         settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
         return settings;
     }
---
+++ src/test/java/ja/workshops/hibernate/parts/queries/query/HQLQueryTest.java
@@ -0,0 +1,53 @@
+package ja.workshops.hibernate.parts.queries.query;
+
+import ja.workshops.hibernate.parts.connectors.ConnectorManager;
+import ja.workshops.hibernate.parts.connectors.H2Connector;
+import ja.workshops.hibernate.parts.connectors.SessionInitializationException;
+import ja.workshops.hibernate.parts.crud.CrudMethods;
+import ja.workshops.hibernate.parts.model.Author;
+import ja.workshops.hibernate.parts.model.Book;
+import ja.workshops.hibernate.parts.model.Genre;
+import org.hibernate.Session;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.Set;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * @author krzysztof.niedzielski
+ */
+public class HQLQueryTest {
+
+
+    private Session session;
+    private IQuery iQuery;
+
+    @BeforeClass
+    public void before() throws SessionInitializationException {
+        Set<Author> authors = Set.of(new Author("Jan", "Brzechwa"), new Author("Julian", "Tuwim"),new Author("Kamil","Rojek"));
+        Book book = new Book("BOOK", authors, Genre.CLASSIC);
+        ConnectorManager.connect(new H2Connector())
+                .openCrudSession(new CrudMethods())
+                .updateRecord(authors)
+                .updateRecord(book)
+                .commitAndClose();
+        this.session = ConnectorManager.connect(new H2Connector("update")).getSession();
+        iQuery= new HQLQuery();
+    }
+
+    @Test(priority = 2)
+    public void testListAll() {
+        List<Author> list = iQuery.listAll(this.session, Author.class);
+        assertEquals( list.size(),3);
+    }
+
+    @Test(priority = 3)
+    public void testListAllAuthorsWithSpecifiedName() {
+        List<Author> list = iQuery.listAllAuthorsWithSpecifiedName(this.session,"Kamil");
+        assertEquals( list.size(),1);
+        session.close();
+    }
+}
