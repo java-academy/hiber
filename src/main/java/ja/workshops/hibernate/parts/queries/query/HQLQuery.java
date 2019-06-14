0e3f0c1572175bb5a59b1b280cde64789ab66525
Krzysiek <krzysztof.niedzielski96@gmail.com>
2019-06-14 20:04:14 +0200

    add queries task: (#27)
    - add query package with 4 different query types
    - add simple tests to query types classes
    - add app
    - add readme.adoc
    - add string field to h2connector class to modify of ddl-auto behaviour
    
    
    Zadanie pokazujące różne sposoby na wysyłanie zapytań do bazy danych poprzez obiekt sesji
    
    SessionFactory.getSession()
    
    W paczce query znajdują się 4 klasy:
    
        CriteriaApi
        HQLQuery
        NamedQuery
        SQLQuery
    
    W każdej klasie znajdują się 2 metody:
    
        listAll
        listAllAuthorsWithSpecifiedName
    
    Do każdej z tych klas dostarczone są proste zestawy testowe, więc po implementacji istnieje mozliwosc szybkiej weryfikacji wyników. W kazdej z klas znajdują się odnośniki do dokumentacji oraz metody na które warto zwrócić uwagę w kontekscie danego sposobu generowania kwerend.
    
    Osoba która przerobi ćwiczenie potrafi:
    
        Wymienić 4 sposoby na tworzenie kwerend używając Hibernate
        Zaimplementować proste kwerendy wybierające

---
+++ src/main/java/ja/workshops/hibernate/parts/queries/query/HQLQuery.java
@@ -0,0 +1,27 @@
+package ja.workshops.hibernate.parts.queries.query;
+
+import ja.workshops.hibernate.parts.model.Author;
+import org.hibernate.Session;
+
+import java.util.List;
+
+/**
+ * Documentation:
+ * Session : https://docs.jboss.org/hibernate/orm/5.4/javadocs/
+ * Tip: createQuery()
+ * @author krzysztof.niedzielski
+ */
+public class HQLQuery implements  IQuery {
+
+    @Override
+    public <T> List<T> listAll(Session session, Class T ){
+        // TODO: Implement me!
+        return null;
+    }
+
+    @Override
+    public List<Author> listAllAuthorsWithSpecifiedName(Session session, String name) {
+        // TODO: Implement me!
+        return null;
+    }
+}
