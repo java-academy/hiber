398e297223967bd88cd1167136b891d9736b1ac8
Agnieszka <atrzewik9412@gmail.com>
2019-06-18 08:59:39 +0200

    Correct readme and package name

--- src/test/java/ja/workshops/hibernate/parts/collections/CollectionsTest.java
+++ src/test/java/ja/workshops/hibernate/parts/collections/CollectionsTest.java
@@ -1,4 +1,4 @@
-package ja.worshops.hibernate.parts.collections;
+package ja.workshops.hibernate.parts.collections;
 
 import ja.workshops.hibernate.parts.model.Author;
 import ja.workshops.hibernate.parts.model.Book;
