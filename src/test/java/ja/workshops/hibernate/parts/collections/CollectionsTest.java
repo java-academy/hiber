e560b57cf8f2958e3a60076cd829a995a30a124c
atrzewik <42387362+atrzewik@users.noreply.github.com>
2019-06-18 14:14:40 +0200

    Collections solution v2 (#47)
    * Complete collections solutions
    * Correct package name
    * Refactor

--- src/test/java/ja/workshops/hibernate/parts/collections/CollectionsTest.java
+++ src/test/java/ja/workshops/hibernate/parts/collections/CollectionsTest.java
@@ -1,9 +1,20 @@
 package ja.workshops.hibernate.parts.collections;
 
+import ja.workshops.hibernate.parts.connectors.H2Connector;
+import ja.workshops.hibernate.parts.connectors.SessionConnector;
 import ja.workshops.hibernate.parts.model.Author;
 import ja.workshops.hibernate.parts.model.Book;
+import ja.workshops.hibernate.parts.model.Genre;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.collection.internal.PersistentSet;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.testng.Assert.assertEquals;
 
 /**
  * @author Agnieszka Trzewik
@@ -13,36 +24,42 @@ public class CollectionsTest {
 
     private Book book;
 
-
     @BeforeMethod
     public void initialize() {
-        //TODO: Implement these authors and add them as the authors of the book:
-        Author author1;
-        Author author2;
-
-        book = new Book();
-
+        Author author1 = new Author("Anna", "Kowalska");
+        Author author2 = new Author("Maria", "Nowak");
+        Set<Author> authors = new HashSet<>();
+        authors.add(author1);
+        authors.add(author2);
+        book = new Book("Fikcyjna podróż", authors, Genre.FANTASY);
     }
 
     @Test
     public void collectionIsOfTypeHashSet() {
-        //TODO: Prove that book.getAuthors() is a java.util.HashSet:
 
         //Arrange
 
         //Act
 
         //Assert
+        assertEquals(book.getAuthors().getClass(), HashSet.class);
     }
 
     @Test
     public void persistingHashSetGivesHibernatePersistentSet() {
-        //TODO: Prove that after book persistence book.getAuthors() is an org.hibernate.collection.internal.PersistentSet:
 
         //Arrange
+        SessionConnector sessionConnector = new H2Connector();
+        Session session = sessionConnector.getSession();
+        Transaction transaction = session.beginTransaction();
 
         //Act
+        session.persist(book);
+
+        transaction.rollback();
+        session.close();
 
         //Assert
+        assertEquals(book.getAuthors().getClass(), PersistentSet.class);
     }
 }
