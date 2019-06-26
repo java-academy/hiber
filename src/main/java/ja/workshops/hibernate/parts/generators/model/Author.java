8af283d3eec85b637f5c23053f9d4a2ed178fbda
Krzysztof Kramarz <krzysztof.kramarz@silvermedia.pl>
2019-06-26 09:39:24 +0200

    refactor readme.adoc

--- src/main/java/ja/workshops/hibernate/parts/generators/model/Author.java
+++ src/main/java/ja/workshops/hibernate/parts/generators/model/Author.java
@@ -10,8 +10,8 @@ import java.util.List;
 public class Author {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.TABLE, generator = "tabela_generator_wartosci")
-    @TableGenerator(name = "tabela_generator_wartosci", initialValue = 50, allocationSize = 100, pkColumnValue = "id_tabela_autor", pkColumnName = "tabele", valueColumnName = "wartosc_id")
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "generator_wartosci")
+    @TableGenerator(name = "generator_wartosci", table = "tabela_generator_wartosci", initialValue = 50, allocationSize = 100, pkColumnValue = "id_tabela_autor", pkColumnName = "tabele", valueColumnName = "wartosc_id")
     @Column(name = "id", updatable = false, nullable = false)
     private Long id;
 
